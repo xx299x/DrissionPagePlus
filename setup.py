1a3bdb20e77d71aaa3ba8b690d58f947468817cf
g1879 <g1879@qq.com>
2020-05-22 00:34:50 +0800

    打包前完善

--- setup.py
+++ setup.py
@@ -3,17 +3,27 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r",encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="DrissionPage",
-    version="1.0.0 Alpha",
+    version="0.8.0",
     author="g1879",
     author_email="g1879@qq.com",
-    description="page contains selenium and requests.",
+    description="A module that integrates selenium and requests session, encapsulates common page operations, "
+                "can achieve seamless switching between the two modes.",
+    long_description=long_description,
     license="BSD",
     keywords="DrissionPage",
-    url="https://gitee.com/g1879/DrissionPage",
+    url="https://github.com/g1879/DrissionPage",
     packages=find_packages(),
-    long_description="Long descrition is actually short...",
+    install_requires=[
+        "selenium",
+        "requests-html",
+        "tldextract",
+        "requests"
+    ],
     classifiers=[
         "Development Status :: 1 - Alpha",
         "Topic :: Utilities",
