bbbf26b97f2de5c45340cea07db9da075c82c60a
g1879 <g1879@qq.com>
2022-02-08 23:24:06 +0800

    修改文档

--- DrissionPage/mix_page.py
+++ DrissionPage/mix_page.py
@@ -113,7 +113,7 @@ class MixPage(SessionPage, DriverPage, BasePage):
 
     def get(self,
             url: str,
-            go_anyway=False,
+            go_anyway=True,
             show_errmsg: bool = False,
             retry: int = None,
             interval: float = None,
