abd6aa707d17fe1344162045b0020b4d44ca94eb
g1879 <g1879@qq.com>
2022-05-16 19:18:08 +0800

    2.6.0，新增Listener类

--- DrissionPage/tools.py
+++ DrissionPage/tools.py
@@ -5,7 +5,7 @@
 from json import loads, JSONDecodeError
 from threading import Thread
 from time import perf_counter, sleep
-from typing import Union, Tuple, List
+from typing import Union, Tuple, List, Iterable
 
 from pychrome import Tab
 
@@ -15,7 +15,13 @@ from .mix_page import MixPage
 
 
 class ResponseData(object):
+    """返回的数据包管理类"""
+
     def __init__(self, response: dict, body: str):
+        """初始化                               \n
+        :param response: response格式化的数据
+        :param body: response包含的内容
+        """
         self.response = response
         self.raw_body = body
         self._json_body = None
@@ -25,6 +31,7 @@ class ResponseData(object):
 
     @property
     def body(self):
+        """返回body内容，如果是json格式，自动进行转换，其它格式直接返回文本"""
         if self._json_body is not False and self.response.get('mimeType', None) == 'application/json':
             if self._json_body is None:
                 try:
@@ -56,13 +63,7 @@ class Listener(object):
 
         self._response_count = 0
         self._requestIds = {}
-
-    # @property
-    # def first_result(self) -> Union[ResponseData, None]:
-    #     try:
-    #         return list(self.results.values())[0][0]
-    #     except IndexError:
-    #         return None
+        self._a_response_loaded = False
 
     def set_targets(self, targets: Union[str, List[str], Tuple[str]]) -> None:
         """设置要拦截的目标，可以设置多个                     \n
@@ -153,7 +154,7 @@ class Listener(object):
         self.tab.Network.responseReceived = self._null_function
         self.tab.Network.loadingFinished = self._null_function
 
-    def _loading_finished(self, **kwargs) -> None:
+    def _loading_finished(self, **kwargs):
         """请求完成时处理方法"""
         requestId = kwargs['requestId']
         target = self._requestIds.pop(requestId, None)
@@ -165,6 +166,28 @@ class Listener(object):
             else:
                 self.results[target] = [response]
             self._response_count += 1
+
+            self._a_response_loaded = True
+
+    def steps(self, response_num: int = 1) -> Iterable:
+        """用于单步操作，可实现没收到若干个数据包执行一步操作（如翻页）                \n
+        于是可以根据数据包是否加载完成来决定是否翻页，无须从页面dom去判断是否加载完成     \n
+        大大简化代码，提高可靠性                                                 \n
+        eg: for i in listener.steps(2):                                      \n
+                btn.click()                                                  \n
+        :param response_num: 每接收到多少个数据包触发
+        :return: None
+        """
+        count = 0
+        while True:
+            if not self.listening:
+                return
+
+            if self._a_response_loaded:
+                self._a_response_loaded = False
+                count += 1
+                if count % response_num == 0:
+                    yield
 
     def _response_received(self, **kwargs) -> None:
         """接收到返回信息时处理方法"""
